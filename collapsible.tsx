feb43602734fe39793a42251d5141c490bcb71e5
Astronaat <natblasalves@gmail.com>
2019-04-30 18:10:18 +0100

    Create collapsible.tsx

---
+++ collapsible.tsx
@@ -0,0 +1,8 @@
+<Collapsible
+    className="more-information"
+    header="More Information"
+    isCollapsed={!this.state.isExpanded}
+    collapsedChanged={this.onCollapseChange} >
+
+    {moreInformation}
+</Collapsible>
